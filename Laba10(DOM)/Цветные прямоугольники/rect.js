716588b5b6af7b49f4a0985598b574a488b1e0ab
JaRiex11 <bolotov3sta@gmail.com>
2024-04-16 22:58:20 +0800

    Начал делать события таймера

--- Laba10(DOM)/Цветные прямоугольники/rect.js
+++ Laba10(DOM)/Цветные прямоугольники/rect.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     // функция будет вызвана один раз после полной загрузки страницы
     window.addEventListener('load', init);
     // здесь нет доступа к элементам DOM, страница еще не загружена
-    
+
     function init() {
         document.getElementById("start").addEventListener("click", okayKlick);
         document.getElementById("stop").addEventListener("click", stopKlick);
     }
 
     //let par = document.getElementById("rectanglearea");
-    for(let i = 0; i < N; i++){
+    for (let i = 0; i < N; i++) {
         let child1 = document.createElement("p");
         child1.setAttribute("class", "rectangle");
         child1.style.backgroundColor = "rgb(" + parseInt(Math.random() * 255) + ", " + parseInt(Math.random() * 255) + ", " + parseInt(Math.random() * 255) + ")";
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("rectanglearea").appendChild(child1);
     }
 
-    
+
 
     function okayKlick() {
         console.log("okayKlick is started");
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function stopKlick() {
         console.log("stopKlick is started");
-        if(flag = true){
+        if (flag = true) {
             console.log("Попали в цикл");
             clearInterval(timer);
             document.getElementById("start").removeAttribute("disabled");
@@ -46,13 +46,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function setRdmColor() {
         let blocks = document.getElementsByClassName('rectangle');
-        for(let i = 0; i < blocks.length; i++){
+        for (let i = 0; i < blocks.length; i++) {
             blocks[i].style.backgroundColor = "rgb(" + parseInt(Math.random() * 255) + ", " + parseInt(Math.random() * 255) + ", " + parseInt(Math.random() * 255) + ")";
         }
     }
 
     let blocks = document.getElementsByClassName('rectangle');
-    for(let i = 0; i < blocks.length; i++){
+
+    for (let i = 0; i < blocks.length; i++) {
         blocks[i].addEventListener("click", removeRect);
         blocks[i].addEventListener("mouseover", mouseIn);
         blocks[i].addEventListener("mouseout", mouseOut);
@@ -63,14 +64,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function mouseIn() {
-        timer2 = setInterval(rotRect, 1000);
+        timer2 = setInterval(rotRect, 1000, this);
     }
+    let i = 1;
     function mouseOut() {
-
+        clearInterval(timer2);
+        this.style.rotate = 0 + "deg";
+        i = 1;
     }
 
-    function rotRect() {
-        this.style.transform = "rotate(10deg)";
+    function rotRect(let) {
+        let.style.rotate = 10 * i + "deg";
+        i++;
+        if (i > 361) i = 1;
+        //console.log("i = " + i);
     }
 });
 
