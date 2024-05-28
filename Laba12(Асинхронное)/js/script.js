b28a878bf44094ef191f935700887212d7628118
Stells228 <tifani.lendosh@yandex.ru>
2024-05-28 11:56:38 +0800

    Доделал асинхронное

--- Laba12(Асинхронное)/js/script.js
+++ Laba12(Асинхронное)/js/script.js
@@ -1,33 +1,135 @@
-let mydata
+let mydata;
 fetch('data/classics.json')
     .then(response => response.json())
     .then(jsonData => mydata = jsonData);
+let cosmodata;
+fetch('data/astronauts.json')
+    .then(response => response.json())
+    .then(jsonData => cosmodata = jsonData);
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    let astroIterator;
+
     window.addEventListener('load', () => {
-        document.getElementById('download').addEventListener('click', loadButton);
+        document.getElementById('download').addEventListener('click', loadButton); // книжки
+        document.getElementById('loadAstro').addEventListener('click', astroLoad);
     });
 
-    function loadButton() {
+    function loadButton() { //книги и их авторы
         console.log(mydata[0].bibliography.title);
 
-        let child = document.createElement("p");
-        let text = '';
-        let br = document.createElement('br');
+        let child = document.createElement("ol");
 
-        for (let i = 0; i < 100; i++) {
-            /*text += mydata[i].bibliography.title;
-            text += '\n';*/
-            child.innerHTML += '(' + mydata[i].bibliography.author.name + ') '
-            child.innerHTML += mydata[i].bibliography.title + '<br>';
+        for (let i = 0; i < 50; i++) {
+            let child2 = document.createElement('li');
+            let child3 = document.createElement('p');
+
+            child3.innerHTML += '(' + mydata[i].bibliography.author.name + ') '
+            child3.innerHTML += mydata[i].bibliography.title;
             console.log(mydata[i].bibliography.title);
             console.log("i = " + i);
+            child2.appendChild(child3) //li
+            child2.setAttribute('id', i);
+            child2.addEventListener('click', urlout);
+            child.appendChild(child2);//ol
         }
         console.log(child);
-        //child.textContent = text;
 
         document.getElementById('first').appendChild(child);
+    }
+
+    let lastIndexOfSelected = -1;
+
+    function urlout() {
+        console.log(this.id);
+        this.setAttribute('class', 'selected');
+        if(lastIndexOfSelected !== -1){
+            document.getElementById(lastIndexOfSelected).removeAttribute('class');
+        }
+        
+
+        let superChild = document.createElement('ul');
+        superChild.setAttribute('id', 'info-container');
+        let child = document.createElement('p');
+        child.innerHTML += "Информация о выбранной книге:" + '<br>';
+        child.innerHTML += "Ссылка на gutenberg.org:" + '<br>';
+        child.innerHTML += mydata[this.id].metadata.url + '<br>';;
+        child.innerHTML += "Статистика:" + '<br>' + "Количество слов: " + mydata[this.id].metrics.statistics.words + '<br>';
+        child.innerHTML += "Количество символов: " + mydata[this.id].metrics.statistics.characters + '<br>';
+        child.innerHTML += "Количество предложений: " + mydata[this.id].metrics.statistics.sentences + '<br>';
+        
+        if(lastIndexOfSelected !== -1){
+            document.getElementById('url-container').removeChild(document.getElementById('info-container'));
+        } else {
+            document.getElementById('url-container').removeChild(document.getElementById('infoAboutBook'));
+        }
+        superChild.appendChild(child);
+        document.getElementById('url-container').appendChild(superChild);
+
+        lastIndexOfSelected = this.id;
+
+    }
+
+    function astroLoad() {
+        console.log(cosmodata.length);
+        astroIterator = 10;
+        outputAstro();
+    }
+
+    function outputAstro() {
+        let child = document.createElement("p");
+        child.setAttribute('id', 'astronauts');
+
+        for (let i = astroIterator - 10; i < astroIterator; i++) {
+            child.innerHTML += (i + 1) + ': ' + cosmodata[i].Profile.Nationality + ' ';
+            child.innerHTML += cosmodata[i].Profile.Name + '<br>';
+            console.log(cosmodata[i].Profile.Name);
+            console.log("i = " + i);
+        }
+
+        document.getElementById('astro-container').appendChild(child);
+        let but1 = document.createElement('button');
+        but1.setAttribute('id', "lastPage");
+        
+        but1.addEventListener('click', lastPage);
+        but1.textContent = "Назад";
+        if(astroIterator <= 10){
+            but1.setAttribute('disabled', 'disabled');
+        } else {
+            but1.removeAttribute('disabled');
+        }
+
+        let but2 = document.createElement('button');
+        but2.setAttribute('id', "nextPage");
+        but2.addEventListener('click', nextPage);
+        but2.textContent = 'Вперёд';
+        if(astroIterator >= cosmodata.length){
+            but2.setAttribute('disabled', 'disabled');
+        } else {
+            but2.removeAttribute('disabled');
+        }
+
+        document.getElementById('astro-container').appendChild(but1);
+        document.getElementById('astro-container').appendChild(but2);
+    }
+
+    function lastPage() {
+        astroIterator -= 10;
+        let cont = document.getElementById('astro-container');
+        cont.removeChild('p');
+        cont.removeChild('button');
+        outputAstro();
+    }
+
+    function nextPage() {
+        astroIterator += 10;
+        let cont = document.getElementById('astro-container');
+        let child = document.getElementById('astronauts');
+        cont.removeChild(child);
+        cont.removeChild(document.getElementById('nextPage'));
+        cont.removeChild(document.getElementById('lastPage'));
+        outputAstro();
     }
 
 });
